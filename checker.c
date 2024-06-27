7cff2b0cb2c7d9082a4e19611ac6672d394a0542
Poovarasan99 <144001340+Poovarasan99@users.noreply.github.com>
2024-06-27 12:10:34 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,12 +4,14 @@
 #define OUTOFBOUNDARY 0
 #define WITHINRANGE 1
 
+/*Function to check whether the battery paramter is within the range*/
 int checkRange(float data, float min, float max)
 {
  int output = WITHINRANGE;
  if (data > max || data < min)
  {
   output = OUTOFBOUNDARY;
+  logger(batteryParameterString);
  }
  return output;
 }
@@ -20,30 +22,20 @@ void logger(const char* stringToBePrinted)
  printf("%s out of range!\n",stringToBePrinted);
 }
 
-/*Function to check whether the battery paramter is within the range*/
-int boundaryCheck(const char* batteryParameterString, int checkRangeResult)
-{
- if(checkRangeResult == OUTOFBOUNDARY)
- {
-  logger(batteryParameterString);
- }
- return checkRangeResult;
-}
-
 int batteryIsOk(float temperature, float soc, float chargeRate) 
 {
  int functionResult = 1; 
- if(boundaryCheck("Temperature", checkRange(temperature,0,45)) == OUTOFBOUNDARY)
+ if(checkRange(temperature,0,45,"Temperature") == OUTOFBOUNDARY)
  {
   functionResult = 0;
  }
- else if(boundaryCheck("State of Charge", checkRange(soc,20,80)) == OUTOFBOUNDARY)
+ else if(checkRange(soc,20,80,"State of Charge") == OUTOFBOUNDARY)
  {
   functionResult = 0;
  }
  else
  {
-  functionResult = boundaryCheck("Charge Rate",checkRange(chargeRate,0,0.8));
+  functionResult = checkRange(chargeRate,0,0.8,Charge Rate");
  }
   return functionResult;
 }
